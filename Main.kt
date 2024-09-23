192da01a17fe6b31ffc7445c8c684eb5c54adb53
Shivam Rai <shivm.jarvis1000@gmail.com>
2024-09-23 08:45:30 +0530

    added new code

--- Main.kt
+++ Main.kt
@@ -1,14 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+fun main(){
+    val tile :TILES  = TILES.yellow("red", 3)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+
+    // The `when` block should be exhaustive
+    val points: Int = when(tile) {
+        is TILES.red -> tile.points * 3
+        is TILES.yellow -> tile.points * 3
+        else -> 0
     }
+
+    println(points)  // This will print the computed points
+}
+
+// Correctly defined sealed class TILES
+sealed class TILES {
+    class red(var colour: String, var points: Int) : TILES()
+    class yellow(var colour: String, var points: Int) : TILES()
 }
